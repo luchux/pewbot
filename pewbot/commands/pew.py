0f1d6dc7cc692334aba0d8e1897557f064a179a7
Brenton Cleeland <b.cleeland@gmail.com>
2013-08-03 22:54:49 +1000

    Closes pull #2, fixed all of @rapilabs SHIITY indentation - use tabs ffs!

--- pewbot/commands/pew.py
+++ pewbot/commands/pew.py
@@ -4,7 +4,11 @@ class Command(PewbotCommand):
     help = "Say pew again, I dare you!"
     def handle(self, message):
         if 'pewbot' not in message: 
-                if 'pew?' in message:
-                        return ["That's right I said pew muthafucka!"]
-                if 'pew' in message:
-                        return ['pew!']
+            if 'pew?' in message:
+                return ["That's right I said pew muthafucka!"]
+            if 'pew' in message:
+                return ['pew!']
+            if 'pork roll' in message:
+                return ['http://s3.amazonaws.com/barkpost-assets/50+GIFs/7.gif', 'Did somebody say pork roll?']
+            if 'coffee' in message:
+                return ['https://lh5.googleusercontent.com/-MlDz2f26eqQ/UY8qz1syY_I/AAAAAAAAA4o/EWq1ENUb71Y/95ff6c96.gif', 'Did somebody say coffee?']
