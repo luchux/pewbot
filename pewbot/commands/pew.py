7a2179083518265349c5e4beb2c3fc7183faf922
Brenton Cleeland <b.cleeland@gmail.com>
2013-08-16 16:12:35 +1000

    Pew commain

--- pewbot/commands/pew.py
+++ pewbot/commands/pew.py
@@ -4,11 +4,5 @@ class Command(PewbotCommand):
     help = "Say pew again, I dare you!"
     def handle(self, message, room_id):
         if 'pewbot' not in message: 
-            if 'pew?' in message:
-                return ["That's right I said pew muthafucka!"]
             if 'pew' in message:
                 return ['pew!']
-            if 'pork roll' in message:
-                return ['http://s3.amazonaws.com/barkpost-assets/50+GIFs/7.gif', 'Did somebody say pork roll?']
-            if 'coffee' in message:
-                return ['https://lh5.googleusercontent.com/-MlDz2f26eqQ/UY8qz1syY_I/AAAAAAAAA4o/EWq1ENUb71Y/95ff6c96.gif', 'Did somebody say coffee?']
--- pewbot/commands/pug.py
+++ pewbot/commands/pug.py
@@ -6,4 +6,13 @@ class Command(PewbotCommand):
     help = "pewbot pug me - post a picture of a pug"
     def handle(self, message, room_id):
         if message.startswith('pewbot pug me'):
+            try:
+                response = []
+                num = int(message.split("pewbot pug me")[1])
+                for x in range(num):
+                    response.append(requests.get('https://pugme.herokuapp.com/random').json()['pug'])
+                return response
+            except:
+                # who cares?
+                pass
             return [requests.get('https://pugme.herokuapp.com/random').json()['pug']]
